08135d478cc04e7bd86e036eb5955dcb9cc15bdd
sagarNeosoft <sagar.modak@wwindia.com>
2017-01-09 12:29:10 +0530

    initial commit. project setup.

---
+++ lolyhub/lolyhub/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lolyhub
+//
+//  Created by webwerks on 1/9/17.
+//  Copyright © 2017 webwerks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("hi lolyhub")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
