4027a4645dc086350790ab6050955d19fe9af1ff
sagarNeosoft <sagar.modak@wwindia.com>
2017-01-09 16:10:57 +0530

    login, signup page switch added

---
+++ lolyhub/lolyhub/LoginSignupParentViewController.swift
@@ -0,0 +1,95 @@
+//
+//  LoginSignupParentViewController.swift
+//  lolyhub
+//
+//  Created by webwerks on 1/9/17.
+//  Copyright © 2017 webwerks. All rights reserved.
+//
+
+import UIKit
+
+class LoginSignupParentViewController: UIViewController {
+
+    enum TabIndex : Int {
+        case FirstChildTab = 0
+        case SecondChildTab = 1
+    }
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var contentView: UIView!
+    
+    var currentViewController: UIViewController?
+    lazy var firstChildTabVC: UIViewController? = {
+        let firstChildTabVC = self.storyboard?.instantiateViewController(withIdentifier: "SignupViewController")
+        return firstChildTabVC
+    }()
+    lazy var secondChildTabVC : UIViewController? = {
+        let secondChildTabVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
+        return secondChildTabVC
+    }()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       // segmentedControl.initUI()
+        
+        segmentedControl.selectedSegmentIndex = TabIndex.FirstChildTab.rawValue
+        displayCurrentTab(tabIndex: TabIndex.FirstChildTab.rawValue)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let currentViewController = currentViewController {
+            currentViewController.viewWillDisappear(animated)
+        }
+    }
+    
+    // MARK: - Switching Tabs Functions
+    @IBAction func switchTabs(_ sender: Any) {
+        
+        self.currentViewController!.view.removeFromSuperview()
+        self.currentViewController!.removeFromParentViewController()
+        
+        displayCurrentTab(tabIndex: (sender as AnyObject).selectedSegmentIndex)
+        
+    }
+    
+    
+    
+    func displayCurrentTab(tabIndex: Int){
+        if let vc = viewControllerForSelectedSegmentIndex(index: tabIndex) {
+            
+            self.addChildViewController(vc)
+            vc.didMove(toParentViewController: self)
+            
+            vc.view.frame = self.contentView.bounds
+            self.contentView.addSubview(vc.view)
+            self.currentViewController = vc
+        }
+    }
+    
+    func viewControllerForSelectedSegmentIndex(index: Int) -> UIViewController? {
+        var vc: UIViewController?
+        switch index {
+        case TabIndex.FirstChildTab.rawValue :
+            vc = firstChildTabVC
+        case TabIndex.SecondChildTab.rawValue :
+            vc = secondChildTabVC
+        default:
+            return nil
+        }
+        
+        return vc
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
