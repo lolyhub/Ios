8e7349cf4d938d13e9280268a802232099846de1
sagarNeosoft <sagar.modak@wwindia.com>
2017-01-30 14:09:33 +0530

    app screen background white

--- lolyhub/lolyhub/SearchViewController.swift
+++ lolyhub/lolyhub/SearchViewController.swift
@@ -23,7 +23,7 @@ class SearchViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+            
 
     /*
     // MARK: - Navigation
