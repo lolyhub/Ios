33ded361f4f3f50f610b2783a373fd2b5c84c88d
sagarmodak <sagar.modak@wwindia.com>
2017-05-17 11:22:05 +0530

    switching between tabs for signUp and signIn

--- lolyhub/lolyhub/LoginSignupParentViewController.swift
+++ lolyhub/lolyhub/LoginSignupParentViewController.swift
@@ -10,29 +10,35 @@ import UIKit
 
 class LoginSignupParentViewController: UIViewController {
 
-   
-    @IBOutlet weak var sloganLabel: UILabel!
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var signUpButton: UIButton!
+    enum TabIndex : Int {
+        case FirstChildTab = 0
+        case SecondChildTab = 1
+    }
+    
+    @IBOutlet var segmentedControl: UISegmentedControl!
     @IBOutlet weak var contentView: UIView!
     
-    
+    var currentViewController: UIViewController?
+    lazy var firstChildTabVC: UIViewController? = {
+        let firstChildTabVC = self.storyboard?.instantiateViewController(withIdentifier: LHViewControllers.SignUpViewController.rawValue)
+        return firstChildTabVC
+    }()
+    lazy var secondChildTabVC : UIViewController? = {
+        let secondChildTabVC = self.storyboard?.instantiateViewController(withIdentifier: LHViewControllers.LoginViewController.rawValue)
+        return secondChildTabVC
+    }()
+
     
     override func viewDidLoad() { 
         super.viewDidLoad()
         
-        setupView()
-        
+        setupSegmentedControl()
+        segmentedControl.selectedSegmentIndex = TabIndex.FirstChildTab.rawValue
+        displayCurrentTab(tabIndex: TabIndex.FirstChildTab.rawValue)
     }
-    
-    
-    func setupView() {
-        
-        self.signUpButton.titleLabel?.font = UIFont.setOpenSansBold(size: 20)
-        self.loginButton.titleLabel?.font = UIFont.setOpenSansBold(size: 20)
-        self.sloganLabel.font = UIFont.setOpenSansBold(size: 20)
-       // self.navigationController?.setNavigationBarHidden(false, animated: false)
-        
+   
+    func setupSegmentedControl() {
+        self.segmentedControl.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.black], for: UIControlState.selected)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,28 +49,51 @@ class LoginSignupParentViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) { 
         super.viewWillDisappear(true)
+        if let currentViewController = currentViewController {
+            currentViewController.viewWillDisappear(animated)
+        }
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
-    
-    @IBAction func signUpClicked(_ sender: Any) {
-
-        let signUpView = UIStoryboard.signupViewController()
-        self.navigationController?.pushViewController(signUpView, animated: true)
+  
+    @IBAction func switchTabs(_ sender: Any) {
+        self.currentViewController!.view.removeFromSuperview()
+        self.currentViewController!.removeFromParentViewController()
+        
+        displayCurrentTab(tabIndex: (sender as AnyObject).selectedSegmentIndex)
     }
     
-    
-    @IBAction func loginClicked(_ sender: Any) {
-        
-        let loginView = UIStoryboard.loginViewController()
-        self.navigationController?.pushViewController(loginView, animated: true)
-        
-    }
     
     @IBAction func crossButtonClicked(_ sender: Any) {
         
         self.navigationController?.dismiss(animated: true, completion: nil)
     }
-       
+    
+    
+    func displayCurrentTab(tabIndex: Int){
+        if let vc = viewControllerForSelectedSegmentIndex(index: tabIndex) {
+            
+            self.addChildViewController(vc)
+            vc.didMove(toParentViewController: self)
+            
+            vc.view.frame = self.contentView.bounds
+            self.contentView.addSubview(vc.view)
+            self.currentViewController = vc
+        }
+    }
+    
+    func viewControllerForSelectedSegmentIndex(index: Int) -> UIViewController? {
+        var vc: UIViewController?
+        switch index {
+        case TabIndex.FirstChildTab.rawValue :
+            vc = firstChildTabVC
+        case TabIndex.SecondChildTab.rawValue :
+            vc = secondChildTabVC
+        default:
+            return nil
+        }
+        
+        return vc
+    }
     
     /*
     // MARK: - Navigation
